bb6066b3729016e4cbf878f36f332c9d30f37f1f
Arvtia <arvazanakuzhy@gmail.com>
2025-10-01 23:05:00 +0530

    Add Tailwind CSS, React Router, and Bootstrap Icons; restructure App component

---
+++ src/Components/TextEditorFeature.jsx
@@ -0,0 +1,12 @@
+
+const TextEditorFeature = (children) => {
+   return (
+      <>
+         <div className="p-2 mx-auto max-w-6xl flex gap-2 space-y-1 rounded-md shadow-sm border-t border-t-neutral-200 border-r-neutral-200">
+            {children}
+         </div>
+      </>
+   )
+}
+
+export default TextEditorFeature
